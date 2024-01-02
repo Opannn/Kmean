b4daeeb01390beb543e7715d6c16cd526c66cbd5
Opannn <140341702+Opannn@users.noreply.github.com>
2024-01-02 21:36:18 +0700

    k-mean.py

--- k-mean.py
+++ k-mean.py
@@ -4,56 +4,42 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.cluster import KMeans
 
-st.write("<h1 style='color: blue;'>Unsupervised Learning on Country Data</h1>", unsafe_allow_html=True)
-
+# Load data
 df = pd.read_csv("Country-data.csv")
 
-X = df.drop(["country"], axis=1)
+# Display title
+st.markdown("<h1 style='color: blue;'>Unsupervised Learning on Country Data</h1>", unsafe_allow_html=True)
 
-st.header("isi dataset")
-st.write(X)
+# Display dataset
+st.header("Dataset")
+st.write(df)
 
+# Elbow plot
+st.header("Elbow Curve for Optimal K")
 k_values = list(range(1, 11))
 inertia_values = []
 
 for best_k in k_values:
-    kmeans = KMeans(
-        n_clusters=best_k,
-        init="k-means++",
-        n_init=10,
-        max_iter=300,
-        tol=0.0001,
-        random_state=45,
-    )
-    kmeans.fit(X)
+    kmeans = KMeans(n_clusters=best_k, init="k-means++", n_init=10, max_iter=300, tol=0.0001, random_state=45)
+    kmeans.fit(df.drop(["country"], axis=1))
     inertia_values.append(kmeans.inertia_)
 
-# Plot the elbow curve to find the optimal k value
-plt.plot(k_values, inertia_values, marker="o")
-plt.xlabel("Number of Clusters (k)")
-plt.ylabel("Inertia")
-plt.title("Elbow Curve")
+# Display the Elbow Curve
+fig, ax = plt.subplots()
+ax.plot(k_values, inertia_values, marker="o")
+ax.set(xlabel="Number of Clusters (k)", ylabel="Inertia", title="Elbow Curve")
+st.pyplot(fig)
 
+# Select number of clusters
+st.header("Select Number of Clusters")
+clust = st.slider("Choose the number of clusters:", 2, 10, 3, 1)
 
-st.set_option("deprecation.showPyplotGlobalUse", False)
-elbo_plot = st.pyplot()
+# K-means clustering and visualization
+def perform_kmeans(best_k):
+    kmeans = KMeans(n_clusters=best_k, init="k-means++", n_init=10, max_iter=300, tol=0.0001, random_state=45).fit(df.drop(["country"], axis=1))
+    df["Cluster"] = kmeans.labels_
 
-st.header("Nilai jumlah K")
-clust = st.slider("Pilih jumlah cluster :", 2, 10, 3, 1)
-
-
-def k_means(best_k):
-    kmeans = KMeans(
-        n_clusters=best_k,
-        init="k-means++",
-        n_init=10,
-        max_iter=300,
-        tol=0.0001,
-        random_state=45,
-    ).fit(X)
-    X["Cluster"] = kmeans.labels_
-
-    fig = px.scatter_3d(x=X["child_mort"], y=X["health"], z=X["inflation"], color=X["Cluster"])
+    fig = px.scatter_3d(x=df["child_mort"], y=df["health"], z=df["inflation"], color=df["Cluster"])
 
     fig.update_layout(
         title="K-means Clustering",
@@ -66,7 +52,7 @@ def k_means(best_k):
 
     st.header("Cluster Plot")
     st.plotly_chart(fig)
-    st.write(X)
+    st.write(df)
 
-
-k_means(clust)
+# Perform K-means clustering
+perform_kmeans(clust)
